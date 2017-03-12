2e40f74135ff191c7069a1db690e179f388ddb01
Joe <Jmannina@Joes-MacBook-Pro.local>
2017-03-12 15:33:58 -0500

    Minor Updates

---
+++ BlackJack/HelpController.swift
@@ -0,0 +1,24 @@
+//
+//  HelpController.swift
+//  BlackJack
+//
+//  Created by Joe on 3/12/17.
+//  Copyright © 2017 Jake Dremann. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HelpController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
