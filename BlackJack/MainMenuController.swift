d3edd42d3f65f449ab37ff4593ba26ce4138d812
Joe <Jmannina@Joes-MacBook-Pro.local>
2017-03-12 15:16:52 -0500

    Started setting up the outline of the application. As well as added pictures of card backgrounds and the cards

---
+++ BlackJack/MainMenuController.swift
@@ -0,0 +1,27 @@
+//
+//  MainMenuController.swift
+//  BlackJack
+//
+//  Created by Joe on 3/12/17.
+//  Copyright © 2017 Jake Dremann. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MainMenuController: UIViewController {
+
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
