6afd030a9b5a47929b3f5dbd1c32b4f70d9d77d4
Joe <Jmannina@Joes-MacBook-Pro.local>
2017-03-12 15:48:23 -0500

    test

---
+++ BlackJack/OptionController.swift
@@ -0,0 +1,26 @@
+//
+//  OptionsController.swift
+//  BlackJack
+//
+//  Created by Joe on 3/12/17.
+//  Copyright © 2017 Jake Dremann. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OptionController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
