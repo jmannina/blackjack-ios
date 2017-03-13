8c1f2b1eaeecdb8a4d6fe1dcf8f9a6f9eaf64eee
Joe <Jmannina@Joes-MacBook-Pro.local>
2017-03-12 23:48:56 -0500

    - Added Cards and Total to the Dealers Side. - Added two more Cards to the user. (Will need to add more/find way to hide labels if they are not being used.

--- BlackJack/StartController.swift
+++ BlackJack/StartController.swift
@@ -10,23 +10,62 @@ import Foundation
 import UIKit
 import GameplayKit
 
+var currentCardIndex = Int()
+var currentNumberOfCards = Int()
+
 
 
 class StartController: UIViewController {
     @IBOutlet weak var lblCardOne: UILabel!
     @IBOutlet weak var lblCardTwo: UILabel!
     @IBOutlet weak var lblCardTotal: UILabel!
-   
+    @IBOutlet weak var lblCardThree: UILabel!
+    @IBOutlet weak var lblCardFour: UILabel!
+    @IBOutlet weak var DealerCardOne: UILabel!
+    @IBOutlet weak var DealerCardTwo: UILabel!
+    @IBOutlet weak var DealerTotal: UILabel!
     
     var shuffle = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: Deck) as! [Card]
+    
+    //will return the next card when the user chooses to "Hit"
+    func nextCard() -> Card{
+        
+        return shuffle[currentCardIndex + 1]
+    }
     
     override func viewWillAppear(_ animated: Bool){
         lblCardOne.text = shuffle[0].cardName
         lblCardTwo.text = shuffle[1].cardName
         lblCardTotal.text = String(shuffle[0].value + shuffle[1].value)
+        DealerCardOne.text = shuffle[2].cardName
+        DealerCardTwo.text = shuffle[3].cardName
+        DealerTotal.text = String(shuffle[2].value
+         + shuffle[3].value)
         
+        currentCardIndex = 3
+        currentNumberOfCards = 4
         
     }
+    
+    @IBAction func HitPressed(_ sender: UIButton) {
+        let currentCard = nextCard()
+        if (currentNumberOfCards == 4){
+            
+            lblCardThree.isEnabled = true
+            lblCardThree.text = currentCard.cardName
+            lblCardTotal.text = String((Int(lblCardTotal.text!)! + currentCard.value))
+            currentCardIndex = 4
+            currentNumberOfCards = 5
+        }
+        else if (currentNumberOfCards == 5){
+            lblCardFour.text = currentCard.cardName
+            lblCardTotal.text = String((Int(lblCardTotal.text!)! + currentCard.value))
+            currentCardIndex = 5
+            currentNumberOfCards = 6
+        }
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
